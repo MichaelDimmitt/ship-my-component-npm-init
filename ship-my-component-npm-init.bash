23592e6b307dafde4160c666d42e6ed87bb867ad
michael dimmitt <michaelgdimmitt@gmail.com>
2019-06-12 15:37:30 -0400

    upload bash script

---
+++ ship-my-component-npm-init.bash
@@ -0,0 +1,41 @@
+[ -f package.json ] && rm package.json;
+dir=${PWD##*/}
+read -p "Package publish name: ($dir)" name;
+read -p "Version: (1.0.0)" version;
+read -p "Project description: " description;
+# version='1.0.1'
+# description=''
+
+/usr/bin/expect <<!
+spawn npm init
+expect "package name:"
+send "$name\n" 
+expect "version:"
+send "$version\n" 
+expect "description:"
+send "$description\n" 
+expect "entry point:"
+send "build/index.js\n" 
+expect "test command:"
+send "webpack\n" 
+expect "git repository"
+send "\n" 
+expect "keywords:"
+send "\n" 
+expect "author:"
+send "\n" 
+expect "license:"
+send "\n" 
+expect "Is this O*"
+send "\n"
+expect eof
+!
+
+# send "$name\n" 
+# send "$entryPoint\n" 
+# send "$testCommand\n" 
+# send "$gitRepo\n" 
+# send "$keywords\n" 
+# send "$author\n" 
+# send "$license\n" 
+# send "$isOkay\n"
